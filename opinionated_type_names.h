fedac834c668c7acc26a0a15393cb078b508b50b
Scruffysnake <55917288+scruffysnake@users.noreply.github.com>
2025-02-11 15:24:23 +0000

    Fix <C++23 warnings, changed name of some types

--- opinionated_type_names.h
+++ opinionated_type_names.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <cstdint>
-#include <stdfloat>
 
 // INTERGERS
 typedef int8_t  i8;
@@ -14,26 +13,26 @@ typedef uint32_t u32;
 typedef uint64_t u64;
 
 // "FAST" INTERGERS
-typedef int_fast8_t  f_i8;
-typedef int_fast16_t f_i16;
-typedef int_fast32_t f_i32;
-typedef int_fast64_t f_i64;
+typedef int_fast8_t  i8_f;
+typedef int_fast16_t i16_f;
+typedef int_fast32_t i32_f;
+typedef int_fast64_t i64_f;
 
-typedef uint_fast8_t  f_u8;
-typedef uint_fast16_t f_u16;
-typedef uint_fast32_t f_u32;
-typedef uint_fast64_t f_u64;
+typedef uint_fast8_t  u8_f;
+typedef uint_fast16_t u16_f;
+typedef uint_fast32_t u32_f;
+typedef uint_fast64_t u64_f;
 
 // "SMALL" INTERGERS
-typedef int_least8_t  s_i8;
-typedef int_least16_t s_i16;
-typedef int_least32_t s_i32;
-typedef int_least64_t s_i64;
+typedef int_least8_t  i8_s;
+typedef int_least16_t i16_s;
+typedef int_least32_t i32_s;
+typedef int_least64_t i64_s;
 			 
-typedef uint_least8_t  s_u8;
-typedef uint_least16_t s_u16;
-typedef uint_least32_t s_u32;
-typedef uint_least64_t s_u64;
+typedef uint_least8_t  u8_s;
+typedef uint_least16_t u16_s;
+typedef uint_least32_t u32_s;
+typedef uint_least64_t u64_s;
 
 // ELSE INTERGERS
 typedef intmax_t i_max;
@@ -43,13 +42,14 @@ typedef uintmax_t u_max;
 typedef uintptr_t u_ptr;
 
 // FLOATS
-#if defined(__STDCPP_FLOAT16_T__)
-typedef std::float16_t  f16;
-typedef std::float32_t  f32;
-typedef std::float64_t  f64;
-typedef std::float128_t f128;
+#if __cplusplus >= 202302L
+    #include <stdfloat>
+    typedef std::float16_t  f16;
+    typedef std::float32_t  f32;
+    typedef std::float64_t  f64;
+    typedef std::float128_t f128;
 #else
-typedef float f16;
-typedef double f32;
-typedef long double f64;
+    typedef float f16;
+    typedef double f32;
+    typedef long double f64;
 #endif
